53f5c9f6b925d0e08ca7146300b7dd8b1c9d65c2
Tushar Sharma <tusharsharma2822@gmail.com>
2025-05-22 12:47:22 +0530

    add user registration and login handlers in user.controller.js

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,71 @@
+import { validationResult } from "express-validator";
+import userModel from "../models/user.model.js";
+import * as userService from "../services/user.service.js";
+import redisClient from "../services/redis.service.js";
+
+export const createUserController = async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { fullname, email, password } = req.body;
+
+    const user = await userService.createUser({
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
+        email,
+        password
+    })
+
+    const token = await user.generateAuthToken(); 
+
+    return res.status(201).json({ user, token });
+}
+
+export const loginUserController = async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if(!user){
+        return res.status(401).json({ message: "Invalid email or password"})
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({ message: "Invalid email or password"})
+    }
+
+    const token = await user.generateAuthToken();
+
+    return res.status(200).json({ user, token });
+}
+
+export const getProfileController = async (req, res) => {
+    return res.status(200).json(req.user)
+}
+
+export const logoutUserController = async (req, res) => {
+    try {
+        
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+        
+        redisClient.set(token, "logout", "EX", 60*60*24);
+
+        return res.status(200).json({ message: "User Log Out Successfully"})
+
+    } catch (error) {
+        
+        return res.status(400).send(err.message);
+
+    }
+}
