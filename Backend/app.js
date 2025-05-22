f4cab754384af52494e328bfa3707096786d48f9
Tushar Sharma <tusharsharma2822@gmail.com>
2025-05-22 12:38:30 +0530

    update app.js to improve backend functionality and fix minor issues

--- Backend/app.js
+++ Backend/app.js
@@ -2,16 +2,21 @@ import express from "express";
 import "dotenv/config.js";
 import cors from "cors";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import connectToDB from "./db/db.js";
+import userRoutes from "./routes/user.route.js";
 
 connectToDB();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 app.use(morgan('dev'));
 app.use(cors());
+
+app.use("/users", userRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
